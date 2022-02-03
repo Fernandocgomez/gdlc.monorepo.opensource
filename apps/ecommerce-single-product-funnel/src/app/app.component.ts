271b3706f09772be27f5004a35b3f289ce2a6c8e
fernandocgomez <fernandocgomez@live.com>
2022-02-03 17:21:15 -0600

    initial commit

---
+++ apps/ecommerce-single-product-funnel/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'multi-step-funnels-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ecommerce-single-product-funnel';
+}
