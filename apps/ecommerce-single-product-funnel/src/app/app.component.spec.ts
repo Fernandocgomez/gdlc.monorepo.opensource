251a5a6b19a0657681b1d26ae2887ba8065d8e67
fernandocgomez <fernandocgomez@live.com>
2022-02-05 22:55:04 -0600

    format

--- apps/ecommerce-single-product-funnel/src/app/app.component.spec.ts
+++ apps/ecommerce-single-product-funnel/src/app/app.component.spec.ts
@@ -5,20 +5,20 @@ import { AppComponent } from './app.component';
 import { TrackingAngularGoogleTagManagerModule } from '@multi-step-funnels/tracking/angular/google-tag-manager';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [
-        TrackingAngularGoogleTagManagerModule.forRoot({
-            id: 'test_id',
-        }),
-      ],
-    }).compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [AppComponent],
+			imports: [
+				TrackingAngularGoogleTagManagerModule.forRoot({
+					id: 'test_id',
+				}),
+			],
+		}).compileComponents();
+	});
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	it('should create the app', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app).toBeTruthy();
+	});
 });
--- libs/tracking/angular/google-tag-manager/src/lib/tracking-angular-google-tag-manager.module.spec.ts
+++ libs/tracking/angular/google-tag-manager/src/lib/tracking-angular-google-tag-manager.module.spec.ts
@@ -1,23 +1,25 @@
 import { inject, TestBed } from '@angular/core/testing';
 
-import { TrackingAngularGoogleTagManagerModule, TrackingGoogleTagManagerService } from '@multi-step-funnels/tracking/angular/google-tag-manager';
+import {
+	TrackingAngularGoogleTagManagerModule,
+	TrackingGoogleTagManagerService,
+} from '@multi-step-funnels/tracking/angular/google-tag-manager';
 
 describe('TrackingGoogleTagManagerService', () => {
-    
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                TrackingAngularGoogleTagManagerModule.forRoot({
-                    id: 'test_id',
-                }),
-            ],
-        });
-    });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				TrackingAngularGoogleTagManagerModule.forRoot({
+					id: 'test_id',
+				}),
+			],
+		});
+	});
 
-    it('should create a TrackingGoogleTagManagerService', inject(
-        [TrackingGoogleTagManagerService],
-        (service: TrackingGoogleTagManagerService) => {
-            expect(service).toBeTruthy();
-        }
-    ))
+	it('should create a TrackingGoogleTagManagerService', inject(
+		[TrackingGoogleTagManagerService],
+		(service: TrackingGoogleTagManagerService) => {
+			expect(service).toBeTruthy();
+		},
+	));
 });
