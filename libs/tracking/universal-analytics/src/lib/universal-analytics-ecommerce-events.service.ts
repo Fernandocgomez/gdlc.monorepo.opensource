d8af2903b5bdd65e83e3e9b77a501a43c48db88d
fernandocgomez <fernandocgomez@live.com>
2022-02-18 16:32:39 -0600

    remove unused interface

--- libs/tracking/universal-analytics/src/lib/universal-analytics-ecommerce-events.service.ts
+++ libs/tracking/universal-analytics/src/lib/universal-analytics-ecommerce-events.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { GoogleTagManagerService } from '@multi-step-funnels/tracking-google-tag-manager';
 
 import {
-	GtmEvent,
 	UniversalAnalyticsEcommerceAddToCartEvent,
 	UniversalAnalyticsEcommerceProductClickEvent,
 	UniversalAnalyticsEcommerceProductImpressionsEvent,
